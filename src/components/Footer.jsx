4cb7fe082303a890d4d98b5bd447960ac5f43758
Rafael Pérez García <rperez.samyroad@gmail.com>
2017-04-14 15:32:32 +0200

    Footer has been styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Spacer from '../components/Spacer.jsx';
 
 const Footer = () => (
   <footer className="Footer">
+    <Spacer />
     <div className="Footer-left">
       <img className="Footer-logo" />
     </div>
     <div className="Footer-right">
-      <p>Some interesting links: WoW Progress | PvE Leaderboards | PvP Leaderboards | WoW Website Enhacer</p>
+      <p>Some interesting links:</p>
+      <ul className="Footer-links">
+        <li className="Footer-link Link">PvE Leaderboards</li>
+        <li className="Footer-link Link">PvP Leaderboards</li>
+        <li className="Footer-link Link">WoW Progress</li>
+        <li className="Footer-link Link">WoW Website Enhacer</li>
+      </ul>
     </div>
   </footer>
 );
