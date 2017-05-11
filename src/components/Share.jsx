81f4bd03b9902fbe30a84a446c6cc8cdce8a3a81
Rafael Pérez <rperez.samyroad@gmail.com>
2017-05-11 14:15:27 +0200

    Share buttons have been implemented

---
+++ src/components/Share.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Icon from '../components/Icon.jsx';
+
+const Share = ({ className }) => {
+  return (
+    <div className={`Share ${className}`}>
+      {/* Facebook */}
+      <button
+        className="Button Button--invisible Button--icon Share-button"
+      >
+        <Icon className="Button-icon Share-buttonIcon" icon="social-facebook" />
+      </button>
+
+      {/* Twitter */}
+      <button
+        className="Button Button--invisible Button--icon Share-button"
+      >
+        <Icon className="Button-icon Share-buttonIcon" icon="social-twitter" />
+      </button>
+
+      {/* Whatsapp TODO: Not included on blizzard icons */}
+      <button
+        className="Button Button--invisible Button--icon Share-button"
+      >
+        <Icon className="Button-icon Share-buttonIcon" icon="social-whatsapp" />
+      </button>
+
+      {/* Share */}
+      <button
+        className="Button Button--invisible Button--icon Share-button"
+      >
+        <Icon className="Button-icon Share-buttonIcon" icon="external" />
+      </button>
+    </div>
+  );
+};
+
+Share.displayName = 'Share';
+
+export default Share;
