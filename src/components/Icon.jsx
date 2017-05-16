1c4b7405c2a27c3b874a8ec72747808c4b1b67a9
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2017-05-16 08:40:14 +0200

    Adding special case for whatsapp icon

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import imageResolver from '../utils/image-resolver.js';
 
-const Icon = ({ className, icon }) => (
-  <svg
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/2000/xlink"
-    viewBox="0 0 64 64"
-  >
-    <use xlinkHref={`${imageResolver('../images/blizzard-icons.svg')}#${icon}`} />
-  </svg>
-);
+const Icon = ({ className, icon }) => {
+  if (icon === 'whatsapp') {
+    return (
+      <svg
+        className={className}
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/2000/xlink"
+        viewBox="0 0 90 90"
+      >
+        <use xlinkHref={`${imageResolver('../images/whatsapp-logo.svg')}#`} />
+      </svg>
+    );
+  }
+
+  return (
+    <svg
+      className={className}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/2000/xlink"
+      viewBox="0 0 64 64"
+    >
+      <use xlinkHref={`${imageResolver('../images/blizzard-icons.svg')}#${icon}`} />
+    </svg>
+  );
+};
 
 export default Icon;
